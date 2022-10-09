f130bf912c5743a1e60fe6b8e85a763d392ba6f3
Jorge Quintana García <alu0101123547@ull.edu.es>
2022-10-09 20:50:41 +0100

    Add files via upload

---
+++ HelloScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GameObject.FindWithTag("Terrain")) {
+            Debug.Log("Terrain says Hello");
+        } 
+        
+        if (GameObject.FindWithTag("MainCamera")) {
+            Debug.Log("MainCamera says Hello");
+        } 
+        
+        if (GameObject.FindWithTag("Light")) {
+            Debug.Log("Light says Hello");
+        } 
+        
+        if (GameObject.FindWithTag("WindZone")) {
+            Debug.Log("WindZone says Hello");
+        } 
+        
+        if (GameObject.FindWithTag("Wall")) {
+            Debug.Log("Wall says Hello");
+        } 
+        
+        if (GameObject.FindWithTag("House")) {
+            Debug.Log("House says Hello");
+        } 
+        
+        if (GameObject.FindWithTag("Barrels")) {
+            Debug.Log("Barrels says Hello");
+        } 
+        
+        if (GameObject.FindWithTag("Sphere")) {
+            Debug.Log("Sphere says Hello");
+        } 
+        
+        if (GameObject.FindWithTag("Tree")) {
+            Debug.Log("Tree says Hello");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
