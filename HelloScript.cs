aa5e8b39cdd74726a84d6136c242fae06ab002a7
Jorge Quintana García <alu0101123547@ull.edu.es>
2022-10-14 10:26:01 +0100

    Update HelloScript.cs

--- HelloScript.cs
+++ HelloScript.cs
@@ -7,41 +7,7 @@ public class HelloScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if (GameObject.FindWithTag("Terrain")) {
-            Debug.Log("Terrain says Hello");
-        } 
-        
-        if (GameObject.FindWithTag("MainCamera")) {
-            Debug.Log("MainCamera says Hello");
-        } 
-        
-        if (GameObject.FindWithTag("Light")) {
-            Debug.Log("Light says Hello");
-        } 
-        
-        if (GameObject.FindWithTag("WindZone")) {
-            Debug.Log("WindZone says Hello");
-        } 
-        
-        if (GameObject.FindWithTag("Wall")) {
-            Debug.Log("Wall says Hello");
-        } 
-        
-        if (GameObject.FindWithTag("House")) {
-            Debug.Log("House says Hello");
-        } 
-        
-        if (GameObject.FindWithTag("Barrels")) {
-            Debug.Log("Barrels says Hello");
-        } 
-        
-        if (GameObject.FindWithTag("Sphere")) {
-            Debug.Log("Sphere says Hello");
-        } 
-        
-        if (GameObject.FindWithTag("Tree")) {
-            Debug.Log("Tree says Hello");
-        }
+        Debug.Log(this.tag + " says Hello");
     }
 
     // Update is called once per frame
